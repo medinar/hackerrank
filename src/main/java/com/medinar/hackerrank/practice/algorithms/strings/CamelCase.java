aad8b034f5ea7b5d508576bd85017816c09e09cf
medinar <rommel.d.medina@gmail.com>
2021-12-16 07:54:44 -0800

    add Solution to Java Inheritance 1

--- src/main/java/com/medinar/hackerrank/practice/algorithms/strings/CamelCase.java
+++ src/main/java/com/medinar/hackerrank/practice/algorithms/strings/CamelCase.java
@@ -1,2 +1,45 @@
-package com.medinar.hackerrank.practice.algorithms.strings;public class CamelCase {
+package com.medinar.hackerrank.practice.algorithms.strings;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+        // Write your code here
+        int wCtr = 1;
+        char[] charArr = s.toCharArray();
+        for (char c : charArr) {
+            if (Character.isUpperCase(c)) {
+                wCtr++;
+            }
+        }
+        return wCtr;
+    }
+
+}
+
+public class CamelCase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+//        String t = "saveChangesInTheEditor";
+//        int result = Result.camelcase(t);
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
